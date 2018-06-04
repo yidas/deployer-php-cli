de12af07e4e533eb767c9eb2e0e19b5a232c39a9
Nick Tsai <myintaer@gmail.com>
2018-06-04 19:13:09 +0800

    Release version 1.3.1

--- src/Deployer.php
+++ src/Deployer.php
@@ -4,7 +4,7 @@
  *
  * Application for deploying projects with management, supporting git and excluding files.
  *
- * @since       1.3.0
+ * @since       1.3.1
  * @author      Nick Tsai <myintaer@gmail.com>
  */
 
