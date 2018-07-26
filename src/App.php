e9d3317ee1c0794599c28ba71bcf618675605a36
Nick Tsai <myintaer@gmail.com>
2018-07-26 14:11:58 +0800

    Release version 1.7.0

--- src/App.php
+++ src/App.php
@@ -3,12 +3,12 @@
 /**
  * Application
  * 
- * @since       1.6.1
+ * @since       1.7.0
  * @author      Nick Tsai <myintaer@gmail.com>
  */
 class App
 {
-    const VERSION = '1.6.1';
+    const VERSION = '1.7.0';
     
     function __construct() 
     {
--- src/Deployer.php
+++ src/Deployer.php
@@ -4,7 +4,7 @@
  *
  * Application for deploying projects with management, supporting git and excluding files.
  *
- * @since       1.6.1
+ * @since       1.7.0
  * @author      Nick Tsai <myintaer@gmail.com>
  */
 
