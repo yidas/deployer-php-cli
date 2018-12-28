439230e2a2d48ef07024cd72b816d41f5fff541e
Nick Tsai <myintaer@gmail.com>
2018-12-28 10:32:01 +0800

    Release version 1.10.1

--- src/App.php
+++ src/App.php
@@ -3,12 +3,12 @@
 /**
  * Application
  * 
- * @since       1.10.0
+ * @since       1.10.1
  * @author      Nick Tsai <myintaer@gmail.com>
  */
 class App
 {
-    const VERSION = '1.10.0';
+    const VERSION = '1.10.1';
     
     function __construct() 
     {
